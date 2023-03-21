1c2f28d3779e4946eb03ad18843f5d7dd7ae3e20
TyronAranez <araneztyron1@gmail.com>
2023-03-21 20:13:59 +0800

    latest 3/21/2023

--- client/src/data/GalleriesData.jsx
+++ client/src/data/GalleriesData.jsx
@@ -1,85 +1,155 @@
 const GalleriesData = [
-  {
-    id: 1,
-    name: "Olivia + John",
-    location: "Enchanted Kingdom",
-    planner: "Dia Leocadio",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595521830354-JY09UM15LD7ROXCZCD7F/610_0067.jpg?format=500w",
-    pageLink: "OliviaJohn",
-  },
-  {
-    id: 2,
-    name: "Liz + Thomas",
-    location: "Manila Cathedral",
-    planner: "Stephen Pagorogon",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1579114748137-QU6U7GJJSSMCFQOAP7S8/TerrainweddingbyPeachPlumPearPhoto_075.jpg",
-    pageLink: "LizThomas",
-  },
-  {
-    id: 3,
-    name: "Becca + Andrew",
-    location: "Intramuros",
-    planner: "Angelica Arcena",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1556746684806-5DACSW12PMVQD3X556TG/SneakPeek-071.jpg",
-    pageLink: "BeccaAndrew",
-  },
-  {
-    id: 4,
-    name: "Theresa + Jamil",
-    location: "Quiapo Church",
-    planner: "Daniel Guevara",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1587663636312-U5SNA4F385ZWLWLR2GCA/2019-12-28+Glenn-Zipf+Wedding-0477.JPG",
-    pageLink: "TheresaJamil",
-  },
-  {
-    id: 5,
-    name: "Megan + Jake",
-    location: "Our Lady of Remedies Parish",
-    planner: "Dia Leocadio",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1562773710566-VMOLY4NSBXLFPDN155M2/chelsea-monte-330.jpg",
-    pageLink: "MeganJake",
-  },
-  {
-    id: 6,
-    name: "Hannah + Carson",
-    location: "San Agustin Church",
-    planner: "Angelica Arcena",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1562768259934-51VOTWM1UWJ1Y77TJI64/sarah-sean-190.jpg",
-    pageLink: "HannahCarson",
-  },
-  {
-    id: 7,
-    name: "Kayleen + Tristan",
-    location: "San Vicente de Paul Parish Church",
-    planner: "Stephen Pagorogon",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1556553431130-ZRS3F3CLWWBQUZTQZNON/Murray_Wedding_488.jpg",
-    pageLink: "KayleenTristan",
-  },
-  {
-    id: 8,
-    name: "Nina + Zach",
-    location: "Minor Basilica of San Sebastian",
-    planner: "Daniel Guevara",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1555089981028-LXFVFOZBP9WSYJWU6I53/chloe-jimmy--142.jpg",
-    pageLink: "NinaZach",
-  },
-  {
-    id: 9,
-    name: "Jess + Jordan",
-    location: "Binondo Church",
-    planner: "Dia Leocadio",
-    thumbnail:
-      "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1556556489912-9U3RGTFTOVPMQFWPBU35/SneakPeek-174.jpg",
-    pageLink: "JessJordan",
-  },
+    {
+        id: 1,
+        name: "Olivia + John",
+        location: "Enchanted Kingdom",
+        planner: "Dia Leocadio",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595521830354-JY09UM15LD7ROXCZCD7F/610_0067.jpg?format=500w",
+        pageLink: "OliviaJohn",
+        images: [
+            {
+                id: 1,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513164486-NRFZKTWBXJXWQ3MQ3KX1/610_0001.jpg",
+            },
+            {
+                id: 2,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513164239-COKFEC326MK2RWB4U2ZJ/610_0002.jpg",
+            },
+            {
+                id: 3,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513192800-LFGUY8TL7HYHVDKKQ1JN/610_0006.jpg",
+            },
+            {
+                id: 4,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513203825-EQZLOL6C03Z510X9LHFB/610_0014.jpg",
+            },
+            {
+                id: 5,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513209371-MOJQ0M00T261F6NZYPHP/610_0019.jpg",
+            },
+            {
+                id: 6,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513252812-ZZ6EKZOVJLF8SX3C8EWD/610_0070.jpg",
+            },
+            {
+                id: 7,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513258941-TFD8PTSBVVJAKB0RTL6F/610_0076.jpg",
+            },
+            {
+                id: 8,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513302442-4HO25FSA608PBBZ4151I/610_0638.jpg",
+            },
+            {
+                id: 9,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513266303-LL57XEGPPYUOTJ55RSJB/610_0091.jpg",
+            },
+            {
+                id: 10,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513271799-0Z8VH0E5LS249KWR23EK/610_0092.jpg",
+            },
+            {
+                id: 11,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513277604-VAULJLD0Q4ZEIC8USFG6/610_0100.jpg",
+            },
+            {
+                id: 12,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513278394-AG0LLX53TF4DAK0WYYNY/610_0101.jpg",
+            },
+            {
+                id: 13,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513313772-9ZP27584I0VP2DCTR5CS/610_0766.jpg",
+            },
+            {
+                id: 14,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513316755-0764CNVYUXNF01E26RWO/610_0927.jpg",
+            },
+            {
+                id: 15,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513244522-TB4EDZGY409T9GSMDX1N/610_0061.jpg",
+            },
+            {
+                id: 16,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513241526-5RN052FMKLQQDRDVW0XC/610_0060.jpg",
+            },
+            {
+                id: 17,
+                imgSrc: "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1595513239958-6BV7LYB5YNYMEFYZ6BH0/610_0045.jpg",
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: "Liz + Thomas",
+        location: "Manila Cathedral",
+        planner: "Stephen Pagorogon",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1579114748137-QU6U7GJJSSMCFQOAP7S8/TerrainweddingbyPeachPlumPearPhoto_075.jpg",
+        pageLink: "LizThomas",
+    },
+    {
+        id: 3,
+        name: "Becca + Andrew",
+        location: "Intramuros",
+        planner: "Angelica Arcena",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1556746684806-5DACSW12PMVQD3X556TG/SneakPeek-071.jpg",
+        pageLink: "BeccaAndrew",
+    },
+    {
+        id: 4,
+        name: "Theresa + Jamil",
+        location: "Quiapo Church",
+        planner: "Daniel Guevara",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1587663636312-U5SNA4F385ZWLWLR2GCA/2019-12-28+Glenn-Zipf+Wedding-0477.JPG",
+        pageLink: "TheresaJamil",
+    },
+    {
+        id: 5,
+        name: "Megan + Jake",
+        location: "Our Lady of Remedies Parish",
+        planner: "Dia Leocadio",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1562773710566-VMOLY4NSBXLFPDN155M2/chelsea-monte-330.jpg",
+        pageLink: "MeganJake",
+    },
+    {
+        id: 6,
+        name: "Hannah + Carson",
+        location: "San Agustin Church",
+        planner: "Angelica Arcena",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1562768259934-51VOTWM1UWJ1Y77TJI64/sarah-sean-190.jpg",
+        pageLink: "HannahCarson",
+    },
+    {
+        id: 7,
+        name: "Kayleen + Tristan",
+        location: "San Vicente de Paul Parish Church",
+        planner: "Stephen Pagorogon",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1556553431130-ZRS3F3CLWWBQUZTQZNON/Murray_Wedding_488.jpg",
+        pageLink: "KayleenTristan",
+    },
+    {
+        id: 8,
+        name: "Nina + Zach",
+        location: "Minor Basilica of San Sebastian",
+        planner: "Daniel Guevara",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1555089981028-LXFVFOZBP9WSYJWU6I53/chloe-jimmy--142.jpg",
+        pageLink: "NinaZach",
+    },
+    {
+        id: 9,
+        name: "Jess + Jordan",
+        location: "Binondo Church",
+        planner: "Dia Leocadio",
+        thumbnail:
+            "https://images.squarespace-cdn.com/content/v1/54b5d529e4b0a17414a81219/1556556489912-9U3RGTFTOVPMQFWPBU35/SneakPeek-174.jpg",
+        pageLink: "JessJordan",
+    },
 ];
 
 export default GalleriesData;
